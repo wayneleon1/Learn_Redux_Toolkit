f9272c19e45a77de946620d931be10ac866abf79
wayneleon1 <rurangwaleon10@gmail.com>
2023-11-06 18:55:30 +0200

    create reducer & slice

---
+++ src/redux/countSlice.jsx
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  value: 0,
+};
+export const counterslice = createSlice({
+  name: count,
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value = +1;
+    },
+    decrement: (state) => {
+      state.value = -1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterslice.actions;
+
+export default counterslice.reducer;
---
+++ src/redux/store.jsx
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../redux/countSlice";
+export const store = configureStore({
+  reducer: {
+    count: counterReducer,
+  },
+});
